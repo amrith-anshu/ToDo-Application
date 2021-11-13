f3cf3af16a6233b445a0a119ee2f7e8649bcbe43
Amrithanshu Kesoju <92728684+amrith-anshu@users.noreply.github.com>
2021-11-13 16:02:02 +0530

    Includes Routing and Http LabWork

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToDoListComponent } from './to-do-list/to-do-list.component';
 import { ToDoDetailsComponent } from './to-do-details/to-do-details.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ToDoDetailsComponent } from './to-do-details/to-do-details.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
